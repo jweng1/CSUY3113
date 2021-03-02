d9a83a61241d91a3d0d0b805767c85300f1f2da1
Jason Weng <jweng2017@gmail.com>
2021-03-01 19:08:26 -0500

    update

--- P2/SDLProject/SDLProject/main.cpp
+++ P2/SDLProject/SDLProject/main.cpp
@@ -19,24 +19,23 @@ ShaderProgram program;
 glm::mat4 viewMatrix, modelMatrix,modelMatrix2,modelMatrix3, projectionMatrix;
 
 
+
 float direction_x = 1.0f;
 float direction_y = 1.0f;
-
-bool start = false;
+bool run = false;
 
 //left paddle
-glm::vec3 left_position = glm::vec3(4.8, 0, 0);
+glm::vec3 left_position = glm::vec3(-4.8, 0, 0);
 glm::vec3 left_movement = glm::vec3(0, 0, 0);
 
 //right paddle
-glm::vec3 right_position = glm::vec3(-4.8, 0, 0);
+glm::vec3 right_position = glm::vec3(4.8, 0, 0);
 glm::vec3 right_movement = glm::vec3(0, 0, 0);
 
-//ball
 glm::vec3 ball_position = glm::vec3(0, 0, 0);
 glm::vec3 ball_movement = glm::vec3(0, 0, 0);
 
-
+float default_speed = 2.0f;
 
 void Initialize() {
     SDL_Init(SDL_INIT_VIDEO);
@@ -80,19 +79,19 @@ void ProcessInput() {
             case SDL_KEYDOWN:
                 switch (event.key.keysym.sym) {
                     case SDLK_LEFT:
-                        // Move the left paddle
                         break;
                     case SDLK_RIGHT:
-                        // Move the right paddle
                         break;
                     case SDLK_SPACE:
-                        start = true;
+                        run = true;
                         break;
                 }
                 break;
         }
     }
+    
     const Uint8 *keys = SDL_GetKeyboardState(NULL);
+    
     if (keys[SDL_SCANCODE_W]) {
         left_movement.y = 2.0f;
     }
@@ -105,54 +104,48 @@ void ProcessInput() {
     if (keys[SDL_SCANCODE_DOWN]) {
         right_movement.y = -2.0f;
     }
-    if (glm::length(ball_movement) > 1.0f) {
-        ball_movement = glm::normalize(ball_movement);
-    }
+
 }
 float lastTicks = 0.0f;
 
 void Update() {
-    //if ball in in bounds
     if (!(ball_position.x >= 4.8f || ball_position.x <= -4.8f)){
         float ticks = (float)SDL_GetTicks() / 1000.0f;
         float deltaTime = ticks - lastTicks;
         lastTicks = ticks;
-
-        left_position += left_movement * deltaTime;
-        right_position += right_position * deltaTime;
+    
+        left_position += left_movement * default_speed * deltaTime;
+        right_position += right_position * default_speed * deltaTime;
         
-        if (left_position.y < -2.75f ){
-            left_position.y = -2.75f;
+        
+        if (left_position.y < -2.55f){
+            left_position.y = -2.55f;
         }
-        if (left_position.y > 2.75f){
-            left_position.y = 2.75f;
+        if (left_position.y > 2.55f){
+            left_position.y = 2.55f;
         }
-        if (right_position.y < -2.75f ){
-            right_position.y = -2.75f;
+        if (right_position.y < -2.55f){
+            right_position.y = -2.55f;
         }
-        if (right_position.y > 2.75f){
-            right_position.y = 2.75f;
+        if (right_position.y > 2.55f){
+            right_position.y = 2.55f;
         }
         
-        if (start){
-            ball_position.x += direction_x * deltaTime;
-            ball_position.y += direction_y * deltaTime;
+        if (run){
+            ball_position.x += direction_x * 2.5 * deltaTime;
+            ball_position.y += direction_y * 2.5 * deltaTime;
         }
-       
-        float rightxdist = fabs(left_position.x - ball_position.x) - ((0.4f + 0.4f) / 2.0f);
-        float rightydist = fabs(left_position.y - ball_position.y) - ((2.0f + 0.4f) / 2.0f);
-        float leftxdist = fabs(right_position.x - ball_position.x) - ((0.4f + 0.4f) / 2.0f);
-        float leftydist = fabs(right_position.y - ball_position.y) - ((2.0f + 0.4f) / 2.0f);
+        float leftxdist = fabs(left_position.x - ball_position.x) - ((0.4f + 0.4f) / 2.0f);
+        float leftydist = fabs(left_position.y - ball_position.y) - ((2.0f + 0.4f) / 2.0f);
+        float rightxdist = fabs(right_position.x - ball_position.x) - ((0.4f + 0.4f) / 2.0f);
+        float rightydist = fabs(right_position.y - ball_position.y) - ((2.0f + 0.4f) / 2.0f);
         
-        if((rightxdist<=0 && rightydist<=0) || (leftxdist <=0 && leftydist <=0)){
+        if ((leftxdist<=0 && leftydist<=0) || (rightxdist <=0 && rightydist <=0)){
             direction_x = -direction_x;
         }
-        
-        if(ball_position.y >= 3.55f || ball_position.y <= -3.55f){
+        if (ball_position.y >= 3.55f || ball_position.y <= -3.55f){
             direction_y = -direction_y;
         }
-         
-        
         modelMatrix = glm::mat4(1.0f);
         modelMatrix = glm::translate(modelMatrix, ball_position);
     }
@@ -172,8 +165,7 @@ void Render() {
     float vertices_p[] = { 0.2f, 1.0f, 0.2f, -1.0f, -0.2f, -1.0f,0.2f, 1.0f,-0.2f, -1.0f,-0.2f,1.0f };
     
     modelMatrix2 = glm::mat4(1.0f);
-    modelMatrix2 =  glm::translate(modelMatrix2,glm::vec3(left_position.x,left_position.y,0.0f));
-
+    modelMatrix2 = glm::translate(modelMatrix2,glm::vec3(right_position.x,right_position.y,0.0f));
     program.SetModelMatrix(modelMatrix2);
 
     glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices_p);
@@ -181,10 +173,10 @@ void Render() {
     glDrawArrays(GL_TRIANGLES, 0, 6);
     glDisableVertexAttribArray(program.positionAttribute);
     
-
     modelMatrix3 = glm::mat4(1.0f);
-        modelMatrix3 = glm::translate(modelMatrix3,glm::vec3(right_position.x,right_position.y,0.0f));
+    modelMatrix3 = glm::translate(modelMatrix3,glm::vec3(left_position.x,left_position.y,0.0f));
     program.SetModelMatrix(modelMatrix3);
+    
     glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices_p);
     glEnableVertexAttribArray(program.positionAttribute);
     glDrawArrays(GL_TRIANGLES, 0, 6);
